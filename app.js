8078ac0156273a0cdb38e02fd54e78d52e53e777
Matt <mw.miles23@gmail.com>
2022-01-25 22:32:17 +0000

    Add, complete and delete functions working

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const todoList = document.querySelector('.todo-list');
 // Event Listeners
 
 todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteCheck)
 
 
 // Functions
@@ -25,7 +26,8 @@ function addTodo(event){
 
     // Create li
     const newTodo = document.createElement('li');
-    newTodo.innerText = 'Hey';
+    // We want the text to be whatever is written in the input form
+    newTodo.innerText = todoInput.value;
     newTodo.classList.add('todo-item');
 
     // Place newTodo li item into the new todoDiv
@@ -46,5 +48,39 @@ function addTodo(event){
     // Now we are adding this new div to the todo ul list defined in the HTML doc
     todoList.appendChild(todoDiv);
 
+    // Clear value in input form
+    todoInput.value = "";
+
+}
+
+function deleteCheck(event) {
+    const item = event.target;
+
+    // Delete TODO
+
+    if(item.classList[0] === 'delete-btn'){
+       // We don't want to delete the item itself (because that is just the trash button)
+       // We want to delete the whole parent item of that button
+        const todo = item.parentElement;
+        // Adding a class to the item to be deleted -> in the stylesheet we are then adding a style to this class so it animates before being deleted
+        todo.classList.add('fall');
+        
+        // Once aninmation has ended, remove the element
+        todo.addEventListener('transitionend', function(){
+            todo.remove()
+        })
+
+       
+    }
+
+    // Check mark
+
+    if(item.classList[0] === 'complete-btn'){
+        // We don't want to chnage the item itself (because that is just the check button)
+        // We want to change the whole parent item of that button
+         const todo = item.parentElement;
+         // We are changing the class of that iten to 'completed'
+         todo.classList.toggle('completed');
+     }
 
 }
